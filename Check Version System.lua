1db4a21dbb20780cdfb94064106255c0a282aa4d
LuaRBXBot <97911525+LuaRBXBot@users.noreply.github.com>
2022-10-17 23:56:29 +0500

    Update Check Version System.lua

--- Check Version System.lua
+++ Check Version System.lua
@@ -1,5 +1,1 @@
-local ver = "v0.6"
-loadstring(game:HttpGet("",true))()
-if Current and Current.Version ~= ver then
-	game:GetService("Players").LocalPlayer:Kick("KukiHUB cannot run.","","Your version: "..ver,"Current version: "..Current.Version,"Run Anyway",true)
-	end
+Current = {Version = "v0.6"}
