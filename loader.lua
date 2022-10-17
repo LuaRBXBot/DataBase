454c539f619207fd9c95a17cf535a4d034e0db0e
LuaRBXBot <97911525+LuaRBXBot@users.noreply.github.com>
2022-10-17 11:27:20 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
 local rest = game:service('Players')
-rul = "Player: "..game.Players.LocalPlayer.Name.." are banned in this ScriptHUB"
+rul = "Player: "... game.Players.LocalPlayer.Name ..." are banned in this ScriptHUB"
 if r[rest.LocalPlayer.UserId] then else 
 rest.LocalPlayer:Kick(rul)end
